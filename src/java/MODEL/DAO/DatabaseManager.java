c70245d60fbb4f866231287d12cfa7e82190f5f8
Unknown <phoogy21@gmail.com>
2019-05-25 19:43:19 +1000

    Updated Database Manager to use new database

--- src/java/MODEL/DAO/DatabaseManager.java
+++ src/java/MODEL/DAO/DatabaseManager.java
@@ -5,12 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.*;
 
-/**
- *
- * @author pramishluitel
- */
 public class DatabaseManager {
 
     private Statement st;
@@ -18,10 +13,61 @@ public class DatabaseManager {
     public DatabaseManager(Connection conn) throws SQLException {
         st = conn.createStatement();
     }
-    User user = new User();
+    
+    // <editor-fold defaultstate="collapsed" desc="User functions. Click on the + sign on the left to edit the code.">
+    
+    public void addUser(String id, String email, String name, String password, String phoneNumber) throws SQLException {
+        st.executeUpdate("insert into \"USER\" ( "
+                + " id"
+                + " ,email"
+                + " ,name"
+                + " ,password"
+                + " ,phonenumber"
+                + " ,status) "
+                + " VALUES("
+                + " '"  + id            + "'"
+                + " ,'" + email         + "'"
+                + " ,'" + name          + "'"
+                + " ,'" + password      + "'"
+                + " ,'" + phoneNumber   + "'"
+                + " ,'ACTIVE')"
+        );    
+    }
+    
+    public User getUser(String id) throws SQLException {
+        String queryString = "select * from \"USER\" where id = '" + id + "'";
+        ResultSet rs = st.executeQuery(queryString);
+        while (rs.next()) {
+            if(id.equals(rs.getString("id")))
+            {
+                return new User(rs.getString("id"), 
+                        rs.getString("email"), 
+                        rs.getString("name"), 
+                        rs.getString("password"), 
+                        rs.getString("phonenumber"), 
+                        rs.getString("status"));
+            }
+        }
+        return null;
+    }
+    
+    public void updateUser(String id, String email, String name, String password, String phoneNumber, String status) throws SQLException {
+        st.executeUpdate("update \"USER\" set "
+                + " email           = '" + email +          "'"
+                + " ,name           = '" + name +           "'"
+                + " ,password       = '" + password +       "'"
+                + " ,phonenumber    = '" + phoneNumber +    "'"
+                + " ,status         = '" + status +         "'"
+                + " where id        = '" + id +             "'"
+        );
+    }
 
+    public void deleteUser(String id) throws SQLException {
+        st.executeUpdate("delete from \"USER\" where id = '" + id + "'");
+    }
+    
     public User findUser(String email, String phoneNumber) throws SQLException {
-        String fetch = "select * from users where email = '" + email + "' and phonenumber = '" + phoneNumber + "'";
+        String fetch = "select * from \"USER\" where email = '" + email + "' and phonenumber = '" + phoneNumber + "'";
         ResultSet rs = st.executeQuery(fetch);
         while (rs.next()) {
             String userEmail = rs.getString(1);
@@ -29,35 +75,20 @@ public class DatabaseManager {
             if (userEmail.equals(email) && phonenumber.equals(phoneNumber)) {
                 String userName = rs.getString(2);
                 String userPass = rs.getString(3);
-                return new User(userEmail, userName, userPass, phonenumber);
+                return new User(rs.getString("id"), 
+                        rs.getString("email"), 
+                        rs.getString("name"), 
+                        rs.getString("password"), 
+                        rs.getString("phonenumber"), 
+                        rs.getString("status"));
             }
         }
         return null;
     }
-
-    public User getUser(String email) throws SQLException {
-        String fetch = "select * from users where email = '" + email + "'";
-        ResultSet rs = st.executeQuery(fetch);
-        while (rs.next()) {
-            String userEmail = rs.getString(1);
-            String userPass = rs.getString(3);
-            if (userEmail.equals(email)) {
-                String userName = rs.getString(2);
-                String phoneNumber = rs.getString(4);
-                return new User(userEmail, userName, userPass, phoneNumber);
-            }
-        }
-        return null;
-    }
-    
-    
-    
-    
-    
 
     public boolean checkUser(String email, String password) throws SQLException {
 //        String query = "Select * from adminlogin Where Username='" + username + "' and Password='" + password + "'";
-        String fetch = "select * from USERS where email='" + email + "'and password='" + password + "'";
+        String fetch = "select * from \"USER\" where email='" + email + "'and password='" + password + "'";
         ResultSet rs = st.executeQuery(fetch);
         while (rs.next()) {
             String userEmail = rs.getString(1);
@@ -70,7 +101,7 @@ public class DatabaseManager {
     }
 
     public boolean checkEmail(String email) throws SQLException {
-        String fetch = "select email from USERS where email='" + email + "'";
+        String fetch = "select email from \"USER\" where email='" + email + "'";
         ResultSet rs = st.executeQuery(fetch);
         while (rs.next()) {
             if (email.equals(rs.getString(1))) {
@@ -79,18 +110,6 @@ public class DatabaseManager {
         }
         return false;
     }
-
-    public void addUser(String email, String name, String password, String phoneNumber) throws SQLException {
-
-        st.executeUpdate("INSERT INTO USERS VALUES('" + email + "','" + name + "','" + password + "','" + phoneNumber + "')");
-    }
-
-    public void deleteUser(String email) throws SQLException {
-        //code for delete-operation
-        st.executeUpdate("delete from users where email = '" + email + "'");
-    }
-
-    public void updateUser(String email, String name, String password, String phoneNumber) throws SQLException {
-        st.executeUpdate("update users set email = '" + email + "', name = '" + name + "', password = '" + password + "' where email = '" + email + "'");
-    }
+    
 }
+    //</editor-fold>
